da705f3ff37f9414fae2bef29541d8ca3d6faa11
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-05-24 13:45:30 -0700

    Colliders added to walls. Camera follows player. Pickup created with Trigger Colliders. Player counts pick ups. Text is displayed for count and win.

--- 2D UFO/Assets/Scripts/PlayerController.cs
+++ 2D UFO/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
     private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -18,5 +26,24 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            ++count;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You win!";
+        }
     }
 }
